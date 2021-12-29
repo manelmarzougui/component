59a017a5b966692077e9b8526231e04994e83628
unknown <manel.marzougui94@gmail.com>
2021-12-29 06:44:50 -0800

    first commit

---
+++ src/profile/Address.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Address() {
+  return (
+    <div className="address">
+      <h4> Sidi Bolbeba Gabes</h4>
+    </div>
+  );
+}
+
+export default Address;
---
+++ src/profile/FullName.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FullName() {
+  return (
+    <div className="name">
+      <h1>Manel Marzougui</h1>
+    </div>
+  );
+}
+
+export default FullName;
